72c2d8ab608c0301b1447e2901feba39f2effd7b
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2013-07-16 10:23:31 +0200

    web ui: add missing translation for Squid component. Refs #1773

---
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Proxy_Squid.php
@@ -0,0 +1,15 @@
+<?php 
+
+/* NethServer_Module_Proxy_Squid translation, language: en */
+
+$L['authenticated_label'] = 'Authenticated';
+$L['manual_label'] = 'Manual';
+$L['Mode_label'] = 'Mode';
+$L['ParentProxy_label'] = 'Parent proxy';
+$L['ProxyAdvanced_label'] = 'Advanced options';
+$L['proxy_label'] = 'proxy';
+$L['Squid_status'] = 'Squid status';
+$L['Squid_Title'] = 'Proxy';
+$L['status_label'] = 'Enable proxy';
+$L['transparent_label'] = 'Transparent';
+$L['transparent_ssl_label'] = 'Transparent with SSL';
---
+++ root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_Proxy_Squid.php
@@ -0,0 +1,15 @@
+<?php 
+
+/* NethServer_Module_Proxy_Squid translation, language: it */
+
+$L['authenticated_label'] = 'Autenticato';
+$L['manual_label'] = 'Manuale';
+$L['Mode_label'] = 'Modalità';
+$L['ParentProxy_label'] = 'Parent proxy';
+$L['ProxyAdvanced_label'] = 'Opzioni avanzate';
+$L['proxy_label'] = 'proxy';
+$L['Squid_status'] = 'Stato Squid';
+$L['Squid_Title'] = 'Proxy';
+$L['status_label'] = 'Abilita proxy';
+$L['transparent_label'] = 'Transparente';
+$L['transparent_ssl_label'] = 'Transparente con SSL';
